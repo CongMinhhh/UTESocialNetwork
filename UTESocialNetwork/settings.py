9c926abc4248714c181c8151d6658bcf634047fd
CongMinhhh <Congminh.00000123@gmail.com>
2025-06-19 14:20:11 +0700

    push

--- UTESocialNetwork/settings.py
+++ UTESocialNetwork/settings.py
@@ -16,8 +16,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-secret-key-for-dev')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'utesocialnetwork.onrender.com').split(',')
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'utesocialnetwork.onrender.com').split(',')
 
 # CSRF_TRUSTED_ORIGINS: phải có khi DEBUG=False và deploy
 CSRF_TRUSTED_ORIGINS = [
